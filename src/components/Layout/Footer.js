5bab1117e0519630c70279b41c6308e24b9128b7
10762 _Ayush chopade <ayushchopade45@gmail.com>
2024-02-10 22:29:41 +0530

    routing functionality added

---
+++ src/components/Layout/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faReact } from "@fortawesome/free-brands-svg-icons";
+const Footer = () => {
+  return (
+    <div className="bg-dark text-light p-3">
+      <h4 className="text-center">  All rights Reserved &copy; </h4>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/pages/Policy.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Layout from '../components/Layout/Layout'
+
+const Policy = () => {
+  return (
+    <Layout>
+
+        <h1>
+            Plociy
+        </h1>
+    </Layout>
+  )
+}
+
+export default Policy
